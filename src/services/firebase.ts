6add6aa6fabb4c003aa3a4593ee157f533825abf
wdeev <wanderson.deev@gmail.com>
2022-06-27 21:12:19 -0300

    criação do projeto e configuração do Firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = new GoogleAuthProvider();
+export const database = getDatabase(app);
